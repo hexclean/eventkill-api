8ec3d692fdba82357d8dc76490749e9d52b9d38a
erdosjozsef <erdos.jozsef24@gmail.com>
2021-10-30 15:35:02 +0300

    new list + username

--- api/auth.js
+++ api/auth.js
@@ -18,157 +18,161 @@ const User = require("../models/User");
 // @desc     Authenticate admin & get token
 // @access   Public
 router.post(
-	"/login",
-	[
-		check("email", "This is not email format").isEmail(),
-		check("password", "Password is required").isLength({ min: 6, max: 30 }),
-	],
-	async (req, res) => {
-		const errors = validationResult(req);
-		if (!errors.isEmpty()) {
-			return res.json({
-				status: 400,
-				msg: "Invalid credentials",
-				result: [],
-			});
-		}
+  "/login",
+  [
+    check("email", "This is not email format").isEmail(),
+    check("password", "Password is required").isLength({ min: 6, max: 30 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.json({
+        status: 400,
+        msg: "Invalid credentials",
+        result: [],
+      });
+    }
 
-		const { email, password } = req.body;
+    const { email, password } = req.body;
 
-		try {
-			let user = await User.findOne({
-				where: { email: email, enabled: 1 },
-			});
+    try {
+      let user = await User.findOne({
+        where: { email: email, enabled: 1 },
+      });
 
-			if (!user) {
-				return res.json({
-					status: 400,
-					msg: "User or password incorrect || User is suspended",
-					result: [],
-				});
-			}
+      if (!user) {
+        return res.json({
+          status: 400,
+          msg: "User or password incorrect || User is suspended",
+          result: [],
+        });
+      }
 
-			const isMatch = await bcrypt.compare(password, user.password);
+      const isMatch = await bcrypt.compare(password, user.password);
 
-			if (!isMatch || !user) {
-				return res.json({
-					status: 400,
-					msg: "User or password incorrect",
-					result: [],
-				});
-			}
+      if (!isMatch || !user) {
+        return res.json({
+          status: 400,
+          msg: "User or password incorrect",
+          result: [],
+        });
+      }
 
-			const payload = {
-				user: {
-					id: user.id,
-					name: user.name,
-					company: user.company,
-				},
-			};
+      const payload = {
+        user: {
+          id: user.id,
+          name: user.name,
+          company: user.company,
+        },
+      };
 
-			jwt.sign(
-				payload,
-				config.get("jwtSecret"),
-				{ expiresIn: "30 days" },
-				(err, token) => {
-					if (err) throw err;
-					res.json({
-						status: 200,
-						msg: "Login success",
-						result: [{ token: token, name: user.name }],
-					});
-				}
-			);
-		} catch (err) {
-			console.log(err);
-			return res.json({
-				status: 500,
-				msg: "Server error",
-				result: [],
-			});
-		}
-	}
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: "30 days" },
+        (err, token) => {
+          if (err) throw err;
+          res.json({
+            status: 200,
+            msg: "Login success",
+            result: [{ token: token, name: user.name }],
+          });
+        }
+      );
+    } catch (err) {
+      console.log(err);
+      return res.json({
+        status: 500,
+        msg: "Server error",
+        result: [],
+      });
+    }
+  }
 );
 
 // @route    POST api/auth/register
 // @desc     Register user
 // @access   Public
 router.post(
-	"/register",
-	[
-		check("email", "This is not email format").isEmail(),
-		check("password", "Password is required").isLength({ min: 6, max: 30 }),
-		check("name", "Name is required").isLength({ min: 2, max: 50 }),
-		check("company", "Company is required").isLength({ min: 1, max: 50 }),
-	],
-	async (req, res) => {
-		const errors = validationResult(req);
+  "/register",
+  [
+    check("email", "This is not email format").isEmail(),
+    check("password", "Password is required").isLength({ min: 6, max: 30 }),
+    check("name", "Name is required").isLength({ min: 2, max: 50 }),
+    check("company", "Company is required").isLength({ min: 1, max: 50 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
 
-		if (!errors.isEmpty()) {
-			return res.json({
-				status: 400,
-				msg: "Invalid credentials",
-				result: [],
-			});
-		}
+    if (!errors.isEmpty()) {
+      return res.json({
+        status: 400,
+        msg: "Invalid credentials",
+        result: [],
+      });
+    }
 
-		const { email, password, name, company, deviceToken } = req.body;
+    const { email, password, name, company, deviceToken } = req.body;
 
-		try {
-			let user = await User.findOne({ where: { email: email } });
+    try {
+      let user = await User.findOne({ where: { email: email } });
 
-			if (user) {
-				return res.json({
-					status: 400,
-					msg: "User already exist",
-					result: [],
-				});
-			}
+      if (user) {
+        return res.json({
+          status: 400,
+          msg: "User already exist",
+          result: [],
+        });
+      }
 
-			user = new User({
-				email,
-				password,
-				name,
-				company,
-				enabled: 1,
-				roleId: 1,
-				deviceToken,
-			});
+      let username = name;
+      username = username.replace(/\s+/g, "").toLowerCase();
 
-			const salt = await bcrypt.genSalt(10);
+      user = new User({
+        email,
+        password,
+        name,
+        company,
+        enabled: 1,
+        roleId: 1,
+        deviceToken,
+        username,
+      });
 
-			user.password = await bcrypt.hash(password, salt);
+      const salt = await bcrypt.genSalt(10);
 
-			await user.save();
+      user.password = await bcrypt.hash(password, salt);
 
-			const payload = {
-				user: {
-					id: user.id,
-				},
-			};
+      await user.save();
 
-			jwt.sign(
-				payload,
-				config.get("jwtSecret"),
-				{ expiresIn: 360000 },
-				(err, token) => {
-					if (err) throw err;
-					res.json({
-						status: 200,
-						msg: "'Registration success",
-						result: [{ token, name }],
-					});
-				}
-			);
-		} catch (err) {
-			console.log(err);
-			return res.json({
-				status: 500,
-				msg: "Server error",
-				result: [],
-			});
-		}
-	}
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({
+            status: 200,
+            msg: "'Registration success",
+            result: [{ token, name }],
+          });
+        }
+      );
+    } catch (err) {
+      console.log(err);
+      return res.json({
+        status: 500,
+        msg: "Server error",
+        result: [],
+      });
+    }
+  }
 );
 
 module.exports = router;
--- api/meets.js
+++ api/meets.js
@@ -21,429 +21,482 @@ const CancelledMeets = require("../models/CancelledMeets");
 //! });
 
 router.get("/people", isAuth, async (req, res) => {
-	try {
-		const users = await User.findAll({
-			where: {
-				id: { [Op.ne]: req.user.id },
-				// status: 10,
-			},
-		});
+  try {
+    const users = await User.findAll({
+      where: {
+        id: { [Op.ne]: req.user.id },
+        // status: 10,
+      },
+    });
 
-		const result = users.map(usr => {
-			return {
-				id: usr.id,
-				name: usr.name + " - " + usr.company,
-			};
-		});
-		console.log(result);
-		return res.json({
-			status: 200,
-			msg: "Today meet list",
-			result: result,
-		});
-	} catch (error) {
-		console.log(error);
-		return res.json({
-			status: 500,
-			msg: "Server error",
-			result: [],
-		});
-	}
+    const result = users.map((usr) => {
+      return {
+        id: usr.id,
+        name: usr.name + " - " + usr.company,
+      };
+    });
+    console.log(result);
+    return res.json({
+      status: 200,
+      msg: "Today meet list",
+      result: result,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.json({
+      status: 500,
+      msg: "Server error",
+      result: [],
+    });
+  }
 });
 
 router.get("/people/:name", isAuth, async (req, res) => {
-	let searchedUser = req.params.name;
-	let result;
-	try {
-		const users = await User.findAll({
-			where: {
-				name: {
-					[Op.like]: `%${req.params.name}%`,
-				},
-				id: { [Op.ne]: req.user.id },
-			},
-		});
+  let searchedUser = req.params.name;
+  let result;
+  try {
+    const users = await User.findAll({
+      where: {
+        name: {
+          [Op.like]: `%${req.params.name}%`,
+        },
+        id: { [Op.ne]: req.user.id },
+      },
+    });
 
-		result = users.map(usr => {
-			return {
-				id: usr.id,
-				name: usr.name + " - " + usr.company,
-			};
-		});
+    result = users.map((usr) => {
+      return {
+        id: usr.id,
+        name: usr.name + " - " + usr.company,
+      };
+    });
 
-		// if (searchedUser.length < 2) {
-		// 	result = {};
-		// }
+    // if (searchedUser.length < 2) {
+    // 	result = {};
+    // }
 
-		// console.log(result);
+    // console.log(result);
 
-		return res.json({
-			status: 200,
-			msg: "Today meet list",
-			result: result,
-		});
-	} catch (error) {
-		console.log(error);
-		return res.json({
-			status: 500,
-			msg: "Server error",
-			result: [],
-		});
-	}
+    return res.json({
+      status: 200,
+      msg: "Today meet list",
+      result: result,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.json({
+      status: 500,
+      msg: "Server error",
+      result: [],
+    });
+  }
 });
 
 // @route    POST api/meets/today
 // @desc     Get all today meets
 // @access   Private
 router.get("/today", isAuth, async (req, res) => {
-	const TODAY_START = new Date().setHours(0, 0, 0, 0);
-	const NOW = new Date();
+  const TODAY_START = new Date().setHours(0, 0, 0, 0);
+  const NOW = new Date();
 
-	try {
-		const meets = await CancelledMeets.findAll({
-			where: {
-				userId: req.user.id,
-				status: 3,
-				// createdAt: {
-				// 	[Op.gt]: TODAY_START,
-				// 	[Op.lt]: NOW,
-				// },
-			},
+  try {
+    const meets = await CancelledMeets.findAll({
+      where: {
+        userId: req.user.id,
+        status: 3,
+        // createdAt: {
+        // 	[Op.gt]: TODAY_START,
+        // 	[Op.lt]: NOW,
+        // },
+      },
 
-			include: [{ model: Meets, include: [{ model: User }] }],
-		});
+      include: [{ model: Meets, include: [{ model: User }] }],
+    });
 
-		const result = meets.map(meet => {
-			return {
-				id: meet.meetId,
-				createdAt: meet.createdAt.toISOString().split("T")[0],
-				status: meet.status,
-				meets: [
-					{
-						title: meet.Meet.title,
-						description: meet.Meet.description,
-						time: meet.Meet.time,
-						startDate: meet.Meet.startDate.toISOString().slice(0, 10),
-					},
-				],
-				partner: [
-					{
-						name: meet.Meet.User.name,
-						email: meet.Meet.User.email,
-						company: meet.Meet.User.company,
-					},
-				],
-			};
-		});
+    const result = meets.map((meet) => {
+      return {
+        id: meet.meetId,
+        createdAt: meet.createdAt.toISOString().split("T")[0],
+        status: meet.status,
+        meets: [
+          {
+            title: meet.Meet.title,
+            description: meet.Meet.description,
+            time: meet.Meet.time,
+            startDate: meet.Meet.startDate.toISOString().slice(0, 10),
+          },
+        ],
+        partner: [
+          {
+            name: meet.Meet.User.name,
+            email: meet.Meet.User.email,
+            company: meet.Meet.User.company,
+          },
+        ],
+      };
+    });
 
-		return res.json({
-			status: 200,
-			msg: "Today meet list",
-			result: result,
-		});
-	} catch (error) {
-		console.log(error);
-		return res.json({
-			status: 500,
-			msg: "Server error",
-			result: [],
-		});
-	}
+    return res.json({
+      status: 200,
+      msg: "Today meet list",
+      result: result,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.json({
+      status: 500,
+      msg: "Server error",
+      result: [],
+    });
+  }
 });
 
 // @route    POST api/meets/pending
 // @desc     Get all pending meets
 // @access   Private
 router.get("/pending", isAuth, async (req, res) => {
-	try {
-		const meets = await CancelledMeets.findAll({
-			where: {
-				userId: req.user.id,
-				mine: 0,
-				status: 0,
-			},
+  try {
+    const meets = await CancelledMeets.findAll({
+      where: {
+        userId: req.user.id,
+        mine: 0,
+        status: 0,
+      },
 
-			include: [{ model: Meets, include: [{ model: User }] }],
-		});
+      include: [{ model: Meets, include: [{ model: User }] }],
+    });
 
-		const result = meets.map(meet => {
-			return {
-				id: meet.meetId,
-				createdAt: meet.createdAt.toISOString().split("T")[0],
-				meets: [
-					{
-						title: meet.Meet.title,
-						description: meet.Meet.description,
-						time: meet.Meet.time,
-						startDate: meet.Meet.startDate.toISOString().slice(0, 10),
-					},
-				],
-				partner: [
-					{
-						name: meet.Meet.User.name,
-						email: meet.Meet.User.email,
-						company: meet.Meet.User.company,
-					},
-				],
-			};
-		});
+    const result = meets.map((meet) => {
+      return {
+        id: meet.meetId,
+        createdAt: meet.createdAt.toISOString().split("T")[0],
+        meets: [
+          {
+            title: meet.Meet.title,
+            description: meet.Meet.description,
+            time: meet.Meet.time,
+            startDate: meet.Meet.startDate.toISOString().slice(0, 10),
+          },
+        ],
+        partner: [
+          {
+            name: meet.Meet.User.name,
+            email: meet.Meet.User.email,
+            company: meet.Meet.User.company,
+          },
+        ],
+      };
+    });
 
-		return res.json({
-			status: 200,
-			msg: "Pending meet list",
-			result: result,
-		});
-	} catch (error) {
-		console.log(error);
-		return res.json({
-			status: 200,
-			msg: "Server error",
-			result: [],
-		});
-	}
+    return res.json({
+      status: 200,
+      msg: "Pending meet list",
+      result: result,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.json({
+      status: 200,
+      msg: "Server error",
+      result: [],
+    });
+  }
 });
 
 // @route    POST api/meets/pending
 // @desc     Get all pending meets
 // @access   Private
 router.get("/accepted", isAuth, async (req, res) => {
-	try {
-		const meets = await CancelledMeets.findAll({
-			where: {
-				userId: req.user.id,
-				// mine: 0,
-				accepted: 1,
-				status: 3,
-			},
+  try {
+    const meets = await CancelledMeets.findAll({
+      where: {
+        userId: req.user.id,
+        // mine: 0,
+        accepted: 1,
+        status: 3,
+      },
 
-			include: [{ model: Meets, include: [{ model: User }] }],
-		});
+      include: [{ model: Meets, include: [{ model: User }] }],
+    });
 
-		const result = meets.map(meet => {
-			return {
-				id: meet.meetId,
-				createdAt: meet.createdAt.toISOString().split("T")[0],
-				meets: [
-					{
-						title: meet.Meet.title,
-						description: meet.Meet.description,
-						time: meet.Meet.time,
-						startDate: meet.Meet.startDate.toISOString().slice(0, 10),
-					},
-				],
-				partner: [
-					{
-						name: meet.Meet.User.name,
-						email: meet.Meet.User.email,
-						company: meet.Meet.User.company,
-					},
-				],
-			};
-		});
+    const result = meets.map((meet) => {
+      return {
+        id: meet.meetId,
+        createdAt: meet.createdAt.toISOString().split("T")[0],
+        meets: [
+          {
+            title: meet.Meet.title,
+            description: meet.Meet.description,
+            time: meet.Meet.time,
+            startDate: meet.Meet.startDate.toISOString().slice(0, 10),
+          },
+        ],
+        partner: [
+          {
+            name: meet.Meet.User.name,
+            email: meet.Meet.User.email,
+            company: meet.Meet.User.company,
+          },
+        ],
+      };
+    });
 
-		return res.json({
-			status: 200,
-			msg: "Pending meet list",
-			result: result,
-		});
-	} catch (error) {
-		console.log(error);
-		return res.json({
-			status: 200,
-			msg: "Server error",
-			result: [],
-		});
-	}
+    return res.json({
+      status: 200,
+      msg: "Pending meet list",
+      result: result,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.json({
+      status: 200,
+      msg: "Server error",
+      result: [],
+    });
+  }
 });
 
 // @route    POST api/meets/declined
 // @desc     Get all pending meets
 // @access   Private
 router.get("/declined", isAuth, async (req, res) => {
-	try {
-		const meets = await CancelledMeets.findAll({
-			where: {
-				userId: req.user.id,
-				// mine: 0,
-				status: 1,
-			},
+  try {
+    const meets = await CancelledMeets.findAll({
+      where: {
+        userId: req.user.id,
+        // mine: 0,
+        status: 1,
+      },
 
-			include: [{ model: Meets, include: [{ model: User }] }],
-		});
+      include: [{ model: Meets, include: [{ model: User }] }],
+    });
 
-		const result = meets.map(meet => {
-			return {
-				id: meet.meetId,
-				createdAt: meet.createdAt.toISOString().split("T")[0],
-				// declinedAt: meet.updatedAt.toISOString().split("T")[0],
-				meets: [
-					{
-						title: meet.Meet.title,
-						description: meet.Meet.description,
-						time: meet.Meet.time,
-						startDate: meet.Meet.startDate.toISOString().slice(0, 10),
-					},
-				],
-				partner: [
-					{
-						name: meet.Meet.User.name,
-						email: meet.Meet.User.email,
-						company: meet.Meet.User.company,
-					},
-				],
-			};
-		});
+    const result = meets.map((meet) => {
+      return {
+        id: meet.meetId,
+        createdAt: meet.createdAt.toISOString().split("T")[0],
+        // declinedAt: meet.updatedAt.toISOString().split("T")[0],
+        meets: [
+          {
+            title: meet.Meet.title,
+            description: meet.Meet.description,
+            time: meet.Meet.time,
+            startDate: meet.Meet.startDate.toISOString().slice(0, 10),
+          },
+        ],
+        partner: [
+          {
+            name: meet.Meet.User.name,
+            email: meet.Meet.User.email,
+            company: meet.Meet.User.company,
+          },
+        ],
+      };
+    });
 
-		return res.json({
-			status: 200,
-			msg: "Pending meet list",
-			result: result,
-		});
-	} catch (error) {
-		console.log(error);
-		return res.json({
-			status: 200,
-			msg: "Server error",
-			result: [],
-		});
-	}
+    return res.json({
+      status: 200,
+      msg: "Pending meet list",
+      result: result,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.json({
+      status: 200,
+      msg: "Server error",
+      result: [],
+    });
+  }
 });
 
 // @route    POST api/meets/calendar
 // @desc     Get all meets
 // @access   Private
 router.get("/calendar", isAuth, async (req, res) => {
-	try {
-		const meets = await CancelledMeets.findAll({
-			where: {
-				userId: req.user.id,
-			},
+  try {
+    const meets = await CancelledMeets.findAll({
+      where: {
+        userId: req.user.id,
+      },
 
-			include: [{ model: Meets, include: [{ model: User }] }],
-		});
+      include: [{ model: Meets, include: [{ model: User }] }],
+    });
 
-		const resultFormat = meets.map(meet => {
-			return {
-				id: meet.meetId,
-				createdAt: meet.createdAt.toISOString().slice(0, 10),
-				startDate: meet.Meet.startDate,
-				status: meet.status,
-				meets: [
-					{
-						title: meet.Meet.title,
-						description: meet.Meet.description,
-						time: meet.Meet.time,
-						startDate: meet.Meet.startDate.toISOString().slice(0, 10),
-					},
-				],
-				partner: [
-					{
-						name: meet.Meet.User.name,
-						email: meet.Meet.User.email,
-						company: meet.Meet.User.company,
-					},
-				],
-			};
-		});
-		console.log(resultFormat);
+    const resultFormat = meets.map((meet) => {
+      return {
+        id: meet.meetId,
+        createdAt: meet.createdAt.toISOString().slice(0, 10),
+        startDate: meet.Meet.startDate,
+        status: meet.status,
+        meets: [
+          {
+            title: meet.Meet.title,
+            description: meet.Meet.description,
+            time: meet.Meet.time,
+            startDate: meet.Meet.startDate.toISOString().slice(0, 10),
+          },
+        ],
+        partner: [
+          {
+            name: meet.Meet.User.name,
+            email: meet.Meet.User.email,
+            company: meet.Meet.User.company,
+          },
+        ],
+      };
+    });
+    console.log(resultFormat);
 
-		const result = resultFormat.reduce(function (r, a) {
-			r[a.startDate.toISOString().slice(0, 10)] =
-				r[a.startDate.toISOString().slice(0, 10)] || [];
-			r[a.startDate.toISOString().slice(0, 10)].push(a);
-			return r;
-		}, Object.create(null));
+    const result = resultFormat.reduce(function (r, a) {
+      r[a.startDate.toISOString().slice(0, 10)] =
+        r[a.startDate.toISOString().slice(0, 10)] || [];
+      r[a.startDate.toISOString().slice(0, 10)].push(a);
+      return r;
+    }, Object.create(null));
 
-		return res.json({
-			status: 200,
-			msg: "Today meet list",
-			result: result,
-		});
-	} catch (error) {
-		console.log(error);
-		return res.json({
-			status: 500,
-			msg: "Server error",
-			result: [],
-		});
-	}
+    return res.json({
+      status: 200,
+      msg: "Today meet list",
+      result: result,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.json({
+      status: 500,
+      msg: "Server error",
+      result: [],
+    });
+  }
 });
 
 // @route    POST api/meets/calendar
 // @desc     Get all meets
 // @access   Private
 router.post("/create", isAuth, async (req, res) => {
-	try {
-		const { title, user, description, date, time } = req.body;
-		console.log(user);
-		const meet = await Meets.create({
-			title,
-			description,
-			userId: req.user.id,
-			statusId: 3,
-			time,
-			startDate: date,
-		});
+  try {
+    const { title, user, description, date, time } = req.body;
+    console.log(user);
+    const meet = await Meets.create({
+      title,
+      description,
+      userId: req.user.id,
+      statusId: 3,
+      time,
+      startDate: date,
+    });
 
-		await CancelledMeets.create({
-			status: 0,
-			userId: req.user.id,
-			meetId: meet.id,
-			mine: 1,
-			accepted: 0,
-		});
+    await CancelledMeets.create({
+      status: 0,
+      userId: req.user.id,
+      meetId: meet.id,
+      mine: 1,
+      accepted: 0,
+    });
 
-		await CancelledMeets.create({
-			status: 0,
-			userId: user.id,
-			meetId: meet.id,
-			mine: 0,
-			accepted: 0,
-		});
+    await CancelledMeets.create({
+      status: 0,
+      userId: user.id,
+      meetId: meet.id,
+      mine: 0,
+      accepted: 0,
+    });
 
-		return res.json({
-			status: 200,
-			msg: "Meet created",
-		});
-	} catch (error) {
-		console.log(error);
-		return res.json({
-			status: 500,
-			msg: "Server error",
-			result: [],
-		});
-	}
+    return res.json({
+      status: 200,
+      msg: "Meet created",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.json({
+      status: 500,
+      msg: "Server error",
+      result: [],
+    });
+  }
 });
 
 // @route    POST api/meets/calendar
 // @desc     Get all meets
 // @access   Private
 router.get("/check/:id", isAuth, async (req, res) => {
-	try {
-		const meet = await CancelledMeets.findAll({
-			where: { meetId: req.params.id, userId: { [Op.ne]: req.user.id } },
-		});
+  try {
+    const meet = await CancelledMeets.findAll({
+      where: { meetId: req.params.id, userId: { [Op.ne]: req.user.id } },
+    });
 
-		const result = meet.map(meet => {
-			return {
-				id: meet.id,
-				status: meet.status,
-			};
-		});
+    const result = meet.map((meet) => {
+      return {
+        id: meet.id,
+        status: meet.status,
+      };
+    });
 
-		return res.json({
-			status: 200,
-			msg: "Meet created",
-			result: result,
-		});
-	} catch (error) {
-		console.log(error);
-		return res.json({
-			status: 500,
-			msg: "Server error",
-			result: [],
-		});
-	}
+    return res.json({
+      status: 200,
+      msg: "Meet created",
+      result: result,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.json({
+      status: 500,
+      msg: "Server error",
+      result: [],
+    });
+  }
+});
+
+// @route    POST api/meets/pending
+// @desc     Get all pending meets
+// @access   Private
+router.get("/sent", isAuth, async (req, res) => {
+  try {
+    const meets = await CancelledMeets.findAll({
+      where: {
+        userId: req.user.id,
+        mine: 1,
+        status: 0,
+        accepted: 0,
+      },
+
+      include: [{ model: Meets, include: [{ model: User }] }],
+    });
+
+    const result = meets.map((meet) => {
+      return {
+        id: meet.meetId,
+        createdAt: meet.createdAt.toISOString().split("T")[0],
+        meets: [
+          {
+            title: meet.Meet.title,
+            description: meet.Meet.description,
+            time: meet.Meet.time,
+            startDate: meet.Meet.startDate.toISOString().slice(0, 10),
+          },
+        ],
+        partner: [
+          {
+            name: meet.Meet.User.name,
+            email: meet.Meet.User.email,
+            company: meet.Meet.User.company,
+          },
+        ],
+      };
+    });
+
+    return res.json({
+      status: 200,
+      msg: "Pending meet list",
+      result: result,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.json({
+      status: 200,
+      msg: "Server error",
+      result: [],
+    });
+  }
 });
 
 module.exports = router;

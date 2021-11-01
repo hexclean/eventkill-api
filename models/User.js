6e43189e8f395e5db7a6c4ffc8a1fb27ce17e35e
erdosjozsef <erdos.jozsef24@gmail.com>
2021-11-01 11:23:57 +0200

    add username

--- models/User.js
+++ models/User.js
@@ -2,35 +2,39 @@ const Sequelize = require("sequelize");
 const sequelize = require("../util/database");
 
 const User = sequelize.define("User", {
-	id: {
-		type: Sequelize.INTEGER,
-		autoIncrement: true,
-		allowNull: false,
-		primaryKey: true,
-	},
-	name: {
-		type: Sequelize.STRING,
-	},
-	email: {
-		type: Sequelize.STRING,
-		allowNull: false,
-	},
-	password: {
-		type: Sequelize.STRING,
-		allowNull: false,
-	},
-	company: {
-		type: Sequelize.STRING,
-	},
-	enabled: {
-		type: Sequelize.INTEGER,
-		allowNull: false,
-	},
-	deviceToken: {
-		type: Sequelize.STRING,
-		allowNull: false,
-	},
-	resetToken: Sequelize.STRING,
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  name: {
+    type: Sequelize.STRING,
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  company: {
+    type: Sequelize.STRING,
+  },
+  enabled: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
+  deviceToken: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  username: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  resetToken: Sequelize.STRING,
 });
 
 module.exports = User;
